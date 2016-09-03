b560715630bd7a1cff546912641a8c18d5e12492
Yuferov Andrey <asjuferov@gmail.com>
2016-09-03 12:50:32 +0300

    sprockets ~> 3.7

--- lib/sprockets/webp/railtie.rb
+++ lib/sprockets/webp/railtie.rb
@@ -4,13 +4,13 @@ module Sprockets
   module WebP
     class Railtie < (::Rails::VERSION::MAJOR < 4 ? ::Rails::Engine : ::Rails::Railtie)
       initializer :webp do |app|
-        app.assets.register_mime_type 'image/jpeg', '.jpg'
-        app.assets.register_postprocessor 'image/jpeg', :jpeg_webp do |context, data|
+        Sprockets.register_mime_type 'image/jpeg', '.jpg'
+        Sprockets.register_postprocessor 'image/jpeg', :jpeg_webp do |context, data|
           Converter.process(app, context, data)
         end
 
-        app.assets.register_mime_type 'image/png', '.png'
-        app.assets.register_postprocessor 'image/png', :png_webp do |context, data|
+        Sprockets.register_mime_type 'image/png', '.png'
+        Sprockets.register_postprocessor 'image/png', :png_webp do |context, data|
           Converter.process(app, context, data)
         end
       end
